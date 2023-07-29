0635eba9140375d5b294e9f9a3ef28fc9da7d0c5
Vilislav Venkov <VVenkov@objectsystems.com>
2023-07-29 14:01:29 +0300

    fix other page

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => {
+const otherPage =  () => {
   return (
     <div>
       Im some other page!
@@ -9,3 +9,5 @@ export default () => {
     </div>
   );
 };
+
+export default otherPage;
